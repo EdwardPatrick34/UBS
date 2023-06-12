a2be1486e16eaeb915f62681408e74aef48eddcc
jonathankl <89733420+jonathankl@users.noreply.github.com>
2023-06-12 19:14:38 +0700

    fix monitoring complain

--- application/models/McompA.php
+++ application/models/McompA.php
@@ -39,12 +39,14 @@ class McompA extends CI_Model{
 	
 
     public function insertcompa($kode_divisi, $no_divisi, $kode_unit, $lokasi_unit, $uraian, $tgl, $jam, $tgljam) {
+		$user = $this->session->userdata('adminNonIT');
+		$usere = $user->USERE;
         $qry = $this->db->query('select * from ed_compA'); 
         $count = $qry->num_rows() + 1; 
         $nocomplain = 'ZM'.substr(date("Y"), 2, 2).str_pad($count, 6, "0", STR_PAD_LEFT);
 
         $sql = "insert into ed_compA(NO_COMPLAIN, TGL, JAM,  KODEDIV, USERE, KODE_UNIT, STATUS, URAIAN) 
-                    values ('$nocomplain', to_date('$tgl', 'yyyy-mm-dd HH:MI:SS'), '$jam', '$kode_divisi', 'usere', '$kode_unit', '1','$uraian')";
+                    values ('$nocomplain', to_date('$tgl', 'yyyy-mm-dd HH:MI:SS'), '$jam', '$kode_divisi', '$usere', '$kode_unit', '1','$uraian')";
         $this->db->query($sql);
 
         return $nocomplain;
--- application/models/MspkA.php
+++ application/models/MspkA.php
@@ -13,12 +13,14 @@ class MspkA extends CI_Model{
     }
 
     public function insertspkA($no_complain, $tgljamspk, $jamspk, $tglspk, $tgljamlapor, $jamlapor, $tgllapor, $usere, $status){
+		$user = $this->session->userdata('admin');
+		$idusere = $user->USERE;
         $qry = $this->db->query("select * from ed_spkA");
         $count = $qry->num_rows() + 1;
         $no_spk = 'ZN'.substr(date("Y"),2,2).str_pad($count, 6 , "0", STR_PAD_LEFT);
 
         $sql = "insert into ed_spkA(NO_SPK, NO_COMPLAIN, TGL_SPK, JAM_SPK, STATUS, TGL_T, JAM_T, USERE) 
-        values ('$no_spk', '$no_complain', to_date('$tglspk', 'yyyy-mm-dd HH:MI:SS'), '$jamspk', '4', to_date('$tgllapor', 'yyyy-mm-dd HH:MI:SS'), '$jamlapor',  '$usere')";
+        values ('$no_spk', '$no_complain', to_date('$tglspk', 'yyyy-mm-dd HH:MI:SS'), '$jamspk', '4', to_date('$tgllapor', 'yyyy-mm-dd HH:MI:SS'), '$jamlapor',  '$idusere')";
 
 
         $this->db->query($sql);
@@ -29,7 +31,20 @@ class MspkA extends CI_Model{
 	//get data Monitoring Complain status SPK
 	public function getMCspk(){
 
-		$sql = $this->db->query("select ED2.NO_COMPLAIN As NO_COMPLAIN, ED2.KODEDIV as KODEDIV, ED2.USERE As PELAPOR, ED1.TGL_SPK As TANGGAL, ED1.JAM_SPK As JAM, ED2.KODE_UNIT As UNIT, ED1.NO_SPK As NO_SPK, ED1.USERE As NAMA_PETUGAS from ED_SPKA ED1 join ED_COMPA ED2 on ED1.NO_COMPLAIN=ED2.NO_COMPLAIN WHERE ED2.STATUS='7'");
+		$sql = $this->db->query("select 
+		ED2.NO_COMPLAIN As NO_COMPLAIN,
+		ED2.KODEDIV as KODEDIV,
+		ED2.USERE As PELAPOR,
+		ED1.TGL_SPK As TANGGAL,
+		ED1.JAM_SPK As JAM,
+		ED2.KODE_UNIT As UNIT,
+		ED1.NO_SPK As NO_SPK,
+		EDP.NAMA_PETUGAS As NAMA_PETUGAS
+		from ED_SPKA ED1 
+		join ED_COMPA ED2 on ED1.NO_COMPLAIN=ED2.NO_COMPLAIN
+		JOIN ED_SPKC EDSC ON ED1.NO_SPK=EDSC.NO_SPK
+		JOIN ED_PETUGAS EDP ON EDSC.PETUGAS=EDP.PETUGAS
+		WHERE ED2.STATUS='7'");
 		
 		return $sql;
 	}
@@ -47,7 +62,19 @@ class MspkA extends CI_Model{
 	public function getMCpending(){
 
 		//where di sesuaikan oleh master status
-		$sql = $this->db->query("select ED2.NO_COMPLAIN As NO_COMPLAIN, ED2.KODEDIV as KODEDIV, ED2.USERE As PELAPOR, ED1.TGL_SPK As TANGGAL, ED1.JAM_SPK As JAM, ED2.KODE_UNIT As UNIT, ED1.NO_SPK As NO_SPK, ED1.USERE As NAMA_PETUGAS from ED_SPKA ED1 join ED_COMPA ED2 on ED1.NO_COMPLAIN=ED2.NO_COMPLAIN WHERE ED2.STATUS=3");
+		$sql = $this->db->query("select ED2.NO_COMPLAIN As NO_COMPLAIN,
+		ED2.KODEDIV as KODEDIV,
+		ED2.USERE As PELAPOR,
+		ED1.TGL_SPK As TANGGAL,
+		ED1.JAM_SPK As JAM,
+		ED2.KODE_UNIT As UNIT,
+		ED1.NO_SPK As NO_SPK,
+		EDP.NAMA_PETUGAS As NAMA_PETUGAS
+		from ED_SPKA ED1 
+		join ED_COMPA ED2 on ED1.NO_COMPLAIN=ED2.NO_COMPLAIN 
+		JOIN ED_SPKC EDSC ON ED1.NO_SPK=EDSC.NO_SPK
+		JOIN ED_PETUGAS EDP ON EDSC.PETUGAS=EDP.PETUGAS
+		WHERE ED2.STATUS=3");
 
 		return $sql;
 
@@ -55,7 +82,19 @@ class MspkA extends CI_Model{
 
 	public function getMCselesai(){
 		//where di sesuaikan oleh master status
-		$sql = $this->db->query("select ED2.NO_COMPLAIN As NO_COMPLAIN, ED2.KODEDIV as KODEDIV, ED2.USERE As PELAPOR, ED1.TGL_SPK As TANGGAL, ED1.JAM_SPK As JAM, ED2.KODE_UNIT As UNIT, ED1.NO_SPK As NO_SPK, ED1.USERE As NAMA_PETUGAS from ED_SPKA ED1 join ED_COMPA ED2 on ED1.NO_COMPLAIN=ED2.NO_COMPLAIN WHERE ED2.STATUS=4");
+		$sql = $this->db->query("select ED2.NO_COMPLAIN As NO_COMPLAIN,
+		ED2.KODEDIV as KODEDIV,
+		ED2.USERE As PELAPOR,
+		ED1.TGL_SPK As TANGGAL,
+		ED1.JAM_SPK As JAM,
+		ED2.KODE_UNIT As UNIT,
+		ED1.NO_SPK As NO_SPK,
+		EDP.NAMA_PETUGAS As NAMA_PETUGAS
+		from ED_SPKA ED1 
+		join ED_COMPA ED2 on ED1.NO_COMPLAIN=ED2.NO_COMPLAIN
+		JOIN ED_SPKC EDSC ON ED1.NO_SPK=EDSC.NO_SPK
+		JOIN ED_PETUGAS EDP ON EDSC.PETUGAS=EDP.PETUGAS
+		WHERE ED2.STATUS=4");
 
 		return $sql;
 
